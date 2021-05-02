433d9a6a703d727441b2830375c904a1a63362ee
michel.santos <michelfreitas85@outlook.com>
2021-05-02 16:22:26 -0300

    Implementado as classes de configuração das entidades para o banco

---
+++ src/MC.ApiCadastroClientes.Infra.Data/EntityConfig/ClienteConfig.cs
@@ -0,0 +1,38 @@
+﻿using MC.ApiCadastroClientes.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MC.ApiCadastroClientes.Infra.Data.EntityConfig
+{
+    public class ClienteConfig : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.ToTable("Clientes");
+
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(prop => prop.Nome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(prop => prop.Cpf)
+                .IsRequired()
+                .HasMaxLength(11)
+                .IsFixedLength();
+
+            builder.Property(prop => prop.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(prop => prop.DataNascimento)
+                .IsRequired();
+
+            builder.Property(prop => prop.Ativo)
+                .IsRequired();
+
+            builder.Property(prop => prop.Excluido)
+                .IsRequired();
+        }
+    }
+}
---
+++ src/MC.ApiCadastroClientes.Infra.Data/EntityConfig/EndercoConfig.cs
@@ -0,0 +1,40 @@
+﻿using MC.ApiCadastroClientes.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MC.ApiCadastroClientes.Infra.Data.EntityConfig
+{
+    class EndercoConfig : IEntityTypeConfiguration<Endereco>
+    {
+        public void Configure(EntityTypeBuilder<Endereco> builder)
+        {
+            builder.ToTable("Enderecos");
+
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(prop => prop.Logradouro)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(prop => prop.Numero)
+                .IsRequired();
+
+            builder.Property(prop => prop.Bairro)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(prop => prop.Cep)
+                .IsRequired()
+                .HasMaxLength(8)
+                .IsFixedLength();
+
+            builder.Property(prop => prop.Complemento)
+                .HasMaxLength(150);
+
+            // ONE TO MANY
+            builder.HasOne(e => e.Cliente)
+                .WithMany(c => c.Enderecos)
+                .HasForeignKey(e => e.ClienteId);
+        }
+    }
+}
