a8e363c9b2e9948e411eac113cc535abf4190abb
michel.santos <michelfreitas85@outlook.com>
2021-05-02 16:23:14 -0300

    Implementando a classe de contexto e configurando

---
+++ src/MC.ApiCadastroClientes.Infra.Data/Context/ApiCadastroClienteContext.cs
@@ -0,0 +1,26 @@
+﻿using MC.ApiCadastroClientes.Domain.Models;
+using MC.ApiCadastroClientes.Infra.Data.EntityConfig;
+using Microsoft.EntityFrameworkCore;
+
+namespace MC.ApiCadastroClientes.Infra.Data.Context
+{
+    public class ApiCadastroClienteContext : DbContext
+    {
+        public ApiCadastroClienteContext(DbContextOptions<ApiCadastroClienteContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Cliente>(new ClienteConfig().Configure);
+
+            modelBuilder.Entity<Endereco>(new EndercoConfig().Configure);
+        }
+    }
+}
