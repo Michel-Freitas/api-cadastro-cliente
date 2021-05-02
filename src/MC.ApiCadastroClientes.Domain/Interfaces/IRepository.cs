eb171f5e7801109b80fbbca469efee8afec1473a
michel.santos <michelfreitas85@outlook.com>
2021-05-02 16:37:28 -0300

    Adicionando a interface generica e a interface do cliente na camada de Domain

---
+++ src/MC.ApiCadastroClientes.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using MC.ApiCadastroClientes.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace MC.ApiCadastroClientes.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        TEntity Adicioner(TEntity obj);
+
+        TEntity ObterPorId(Guid id);
+
+        IEnumerable<TEntity> ObterTodos();
+
+        IEnumerable<TEntity> ObterTodosPaginados(int s, int t);
+
+        TEntity Atualizar(TEntity obj);
+
+        void Remover(Guid id);
+
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate);
+
+        int SaveChanges();
+    }
+}
