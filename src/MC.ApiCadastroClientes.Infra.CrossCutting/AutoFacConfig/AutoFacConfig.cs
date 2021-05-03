6ee006bcf21ff818b87aef1466b630e4a80d217d
michel.santos <michelfreitas85@outlook.com>
2021-05-03 16:49:01 -0300

    Adicionando e configurando o AutoFac

---
+++ src/MC.ApiCadastroClientes.Infra.CrossCutting/AutoFacConfig/AutoFacConfig.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using MC.ApiCadastroClientes.Application.Interfaces;
+using MC.ApiCadastroClientes.Application.Services;
+using MC.ApiCadastroClientes.Domain.Interfaces;
+using MC.ApiCadastroClientes.Infra.Data.Repository;
+
+namespace MC.ApiCadastroClientes.Infra.CrossCutting.AutoFacConfig
+{
+    public class AutoFacConfig
+    {
+        public static void Load(ContainerBuilder builder)
+        {
+            #region AutoFac
+
+            builder.RegisterType<ClienteAppService>().As<IClienteAppService>();
+            builder.RegisterType<ClienteRepository>().As<IClienteRepository>();
+
+            #endregion
+        }
+    }
+}
---
+++ src/MC.ApiCadastroClientes.Infra.CrossCutting/AutoFacConfig/ModuleAutoFac.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace MC.ApiCadastroClientes.Infra.CrossCutting.AutoFacConfig
+{
+    public class ModuleAutoFac : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            AutoFacConfig.Load(builder);
+        }
+    }
+}
