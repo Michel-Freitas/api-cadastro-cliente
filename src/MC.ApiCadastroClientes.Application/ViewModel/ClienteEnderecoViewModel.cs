ec6fb626ee0e435d97600ec463931e74b34827b8
michel.santos <michelfreitas85@outlook.com>
2021-05-03 15:40:42 -0300

    Adicioando os DTOs das entidades

---
+++ src/MC.ApiCadastroClientes.Application/ViewModel/ClienteEnderecoViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MC.ApiCadastroClientes.Application.ViewModel
+{
+    public class ClienteEnderecoViewModel
+    {
+        public ClienteViewModel Cliente { get; set; }
+
+        public EnderecoViewModel Endereco { get; set; }
+    }
+}
