3d2ee29caf99d9093272095e10505cff2ef12881
michel.santos <michelfreitas85@outlook.com>
2021-05-21 14:23:10 -0300

    Criando a interface para o repository de endereco

---
+++ src/MC.ApiCadastroClientes.Domain/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,13 @@
+﻿using MC.ApiCadastroClientes.Domain.Models;
+using System.Collections.Generic;
+
+namespace MC.ApiCadastroClientes.Domain.Interfaces
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        public IEnumerable<Endereco> ObterPorEstado();
+        public IEnumerable<Endereco> ObterPorCidade();
+        public IEnumerable<Endereco> ObterPorBairro();
+        public IEnumerable<Endereco> ObterPorLogradouro();
+    }
+}
