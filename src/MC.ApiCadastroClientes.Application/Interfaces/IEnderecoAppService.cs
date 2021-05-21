79c30ceff8d83058ae9b49f7d52a489feb40407d
michel.santos <michelfreitas85@outlook.com>
2021-05-21 14:22:13 -0300

    Criando a Interface e a classe de EnderecoAppService

---
+++ src/MC.ApiCadastroClientes.Application/Interfaces/IEnderecoAppService.cs
@@ -0,0 +1,16 @@
+﻿using MC.ApiCadastroClientes.Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MC.ApiCadastroClientes.Application.Interfaces
+{
+    public interface IEnderecoAppService
+    {
+        NewEnderecoViewModel adicionar(NewEnderecoViewModel endereco, Guid clienteId);
+
+        IEnumerable<ViewUpdateEnderecoViewModel> listarTodosEnderecos();
+    }
+}
---
+++ src/MC.ApiCadastroClientes.Application/Services/EnderecoAppService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MC.ApiCadastroClientes.Application.Interfaces;
+using MC.ApiCadastroClientes.Application.ViewModel;
+using MC.ApiCadastroClientes.Domain.Interfaces;
+using MC.ApiCadastroClientes.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MC.ApiCadastroClientes.Application.Services
+{
+    public class EnderecoAppService : IEnderecoAppService
+    {
+        private readonly IEnderecoRepository _enderecoRepository;
+        private static IMapper _mapper;
+
+        public EnderecoAppService(IEnderecoRepository enderecoRepository, IMapper mapper)
+        {
+            _enderecoRepository = enderecoRepository;
+            _mapper = mapper;
+        }
+
+        public NewEnderecoViewModel adicionar(NewEnderecoViewModel enderecoModel, Guid clienteId)
+        {
+            var endereco = _mapper.Map<Endereco>(enderecoModel);
+            _enderecoRepository.Adicioner(endereco);
+
+            return enderecoModel;
+        }
+
+        public IEnumerable<ViewUpdateEnderecoViewModel> listarTodosEnderecos()
+        {
+            return _mapper.Map<IEnumerable<ViewUpdateEnderecoViewModel>>(_enderecoRepository.ObterTodos());
+        }
+    }
+}
