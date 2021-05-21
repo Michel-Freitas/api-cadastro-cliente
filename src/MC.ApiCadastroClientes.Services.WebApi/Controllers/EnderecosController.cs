ed6cc74882a57ee0d190bca16be6811be8939a40
michel.santos <michelfreitas85@outlook.com>
2021-05-21 14:19:41 -0300

    Criando o EnderecoController

---
+++ src/MC.ApiCadastroClientes.Services.WebApi/Controllers/EnderecosController.cs
@@ -0,0 +1,36 @@
+﻿using MC.ApiCadastroClientes.Application.Interfaces;
+using MC.ApiCadastroClientes.Application.ViewModel;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MC.ApiCadastroClientes.Services.WebApi.Controllers
+{
+    [EnableCors("MyPolicy")]
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class EnderecosController : ControllerBase
+    {
+        private readonly IEnderecoAppService _enderecoAppService;
+
+        public EnderecosController(IEnderecoAppService enderecoAppService)
+        {
+            _enderecoAppService = enderecoAppService;
+        }
+
+        [HttpPost("{clienteId:guid}")]
+        public ActionResult<NewEnderecoViewModel> adicionarEndereco([FromBody] NewEnderecoViewModel endereco, Guid clienteId)
+        {
+            return _enderecoAppService.adicionar(endereco, clienteId);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ViewUpdateEnderecoViewModel>> listarTodosEnderecos()
+        {
+            return _enderecoAppService.listarTodosEnderecos().ToList();
+        }
+    }
+}
