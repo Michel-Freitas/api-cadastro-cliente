641ad8ee882f766f4bfd90e7a79c815a21935dde
michel.santos <michelfreitas85@outlook.com>
2021-05-30 16:53:41 -0300

    Criando as Validations para o Cliente

---
+++ src/MC.ApiCadastroClientes.Domain/Validations/Clientes/ClienteEstaConsistenteValidation.cs
@@ -0,0 +1,20 @@
+﻿using DomainValidationCore.Validation;
+using MC.ApiCadastroClientes.Domain.Models;
+using MC.ApiCadastroClientes.Domain.Specifications.Clientes;
+
+namespace MC.ApiCadastroClientes.Domain.Validations.Clientes
+{
+    public class ClienteEstaConsistenteValidation : Validator<Cliente>
+    {
+        public ClienteEstaConsistenteValidation()
+        {
+            var CpfCliente = new ClienteDeveSerMaiorDeIdadeSpecification();
+            var EmailCliente = new ClienteDeveTerEmailValidoSpecification();
+            var MaiorIdadeCliente = new ClienteDeveSerMaiorDeIdadeSpecification();
+
+            base.Add("CpfCliente", new Rule<Cliente>(CpfCliente, "Cliente informou o CPF inválido."));
+            base.Add("EmailCliente", new Rule<Cliente>(EmailCliente, "Cliente informou um e-mail inválido."));
+            base.Add("MaiorIdadeCliente", new Rule<Cliente>(MaiorIdadeCliente, "Cliente não tem maioridade para cadastro."));
+        }
+    }
+}
