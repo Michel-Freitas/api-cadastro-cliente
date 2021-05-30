c8072329d70358204b7498f76b95549845b1df82
michel.santos <michelfreitas85@outlook.com>
2021-05-29 21:27:11 -0300

    Criando a interface para o serviço de cliente na camada de Domain

---
+++ src/MC.ApiCadastroClientes.Domain/Interfaces/Services/IClienteService.cs
@@ -0,0 +1,12 @@
+﻿using MC.ApiCadastroClientes.Domain.Models;
+using System;
+
+namespace MC.ApiCadastroClientes.Domain.Interfaces.Services
+{
+    public interface IClienteService : IDisposable
+    {
+        Cliente Adicionar(Cliente cliente);
+        Cliente Atualizar(Cliente cliente);
+        void Remover(Guid id);
+    }
+}
